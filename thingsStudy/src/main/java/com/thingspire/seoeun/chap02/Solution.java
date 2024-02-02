9e21caa47d53cda3288593a0a76aafa6ed73358d
parkseoeun <s2tjdms@thingspire.com>
2024-02-02 12:52:40 +0900

    cimmit test

---
+++ thingsStudy/src/main/java/com/thingspire/seoeun/chap02/Solution.java
@@ -0,0 +1,21 @@
+package com.thingspire.seoeun.chap02;
+
+public class Solution {
+    /*
+    문제 설명
+
+    제한사항
+     */
+    public int solution(int[] array, int height) {
+        int count = 0;
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
